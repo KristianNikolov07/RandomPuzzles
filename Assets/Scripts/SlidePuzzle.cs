c41a207f333975cd5166468de37496cecdd8ecee
KNProNoob <knpronoob@gmail.com>
2025-03-26 13:52:22 +0200

    Added the slide puzzle randomizer, put the puzzle in a room and added the room to the levergen

--- Assets/Scripts/SlidePuzzle.cs
+++ Assets/Scripts/SlidePuzzle.cs
@@ -1,8 +1,13 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SlidePuzzle : MonoBehaviour
 {
+
+    public UnityEvent solved;
+    public UnityEvent unsolved;
     public GameObject[] tiles;
     
     void Awake(){
@@ -78,16 +83,45 @@ public class SlidePuzzle : MonoBehaviour
             if (tile != null){
                 SlidePuzzleTile tileScript = tile.GetComponent<SlidePuzzleTile>();
                 if((tileScript.arrPos + 1).ToString() != tileScript.name){
+                    unsolved.Invoke();
                     return;
                 }
             }
             
             
         }
-        Debug.Log("Solved");
+        solved.Invoke();
     }
 
     void scramble(){
-        //TODO: ADD SCRAMBLE ALG
+        int emptyIndex = 15;
+
+        for(int i = 0; i < 100; i++){
+            int rand = Random.Range(1, 5);
+            if(rand == 1){
+                if(emptyIndex != 0 && emptyIndex != 4 && emptyIndex != 8 && emptyIndex != 12){
+                    tileClicked(emptyIndex - 1);
+                    emptyIndex --;
+                }
+            }
+            if(rand == 2){
+                if(emptyIndex != 3 && emptyIndex != 7 && emptyIndex != 11 && emptyIndex != 15){
+                    tileClicked(emptyIndex + 1);
+                    emptyIndex ++;
+                }
+            }
+            if(rand == 3){
+                if(emptyIndex != 0 && emptyIndex != 1 && emptyIndex != 2 && emptyIndex != 3){
+                    tileClicked(emptyIndex - 4);
+                    emptyIndex -= 4;
+                }
+            }
+            if(rand == 4){
+                if(emptyIndex != 12 && emptyIndex != 13 && emptyIndex != 14 && emptyIndex != 15){
+                    tileClicked(emptyIndex + 4);
+                    emptyIndex += 4;
+                }
+            }
+        }
     }
 }
