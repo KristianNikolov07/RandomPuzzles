fb95127ec188fdb70451ddadbe42acea5bcfc667
KNProNooob <138398165+KNProNooob@users.noreply.github.com>
2025-03-27 23:33:35 +0200

    Added a timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    TextMeshProUGUI meshPro; 
+    float time = 0;
+
+    void Awake()
+    {
+        meshPro = GetComponent<TextMeshProUGUI>();
+
+        
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        float time_rounded = (float)Math.Round(time, 2);
+
+        int totalSeconds = (int)time_rounded;
+        int hours = totalSeconds / 3600;
+        int remainingSeconds = totalSeconds % 3600;
+        int mins = remainingSeconds / 60;
+        int secs = remainingSeconds % 60;
+
+
+        float fractionalPart = time_rounded - totalSeconds;
+        int cents = Mathf.RoundToInt(fractionalPart * 100); 
+
+        meshPro.text = $"{hours:D2}:{mins:D2}:{secs:D2}.{cents:D2}";
+
+    }
+}
