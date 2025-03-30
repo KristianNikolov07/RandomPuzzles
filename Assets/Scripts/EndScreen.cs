eb742ca90a675075bcdbcff49d5387b007446f54
KNProNooob <138398165+KNProNooob@users.noreply.github.com>
2025-03-30 20:29:48 +0300

    Added the code for uploading times to the server

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -1,10 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+
 
 public class EndScreen : MonoBehaviour
 {
+    string url = "localhost:8080";
+    public GameObject nameInput;
     public GameObject TimerText;
+    public GameObject errorText;
     void Start()
     {
         TimerText.GetComponent<TextMeshProUGUI>().text = TimerInfo.timeText;
@@ -14,6 +21,32 @@ public class EndScreen : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UploadScore() {
+        string username = nameInput.GetComponent<TextMeshProUGUI>().text;
+        float time = TimerInfo.time;
+        int numRooms = GameSettings.numRooms;
+        StartCoroutine(Upload(username, time, numRooms));
+    }
+
+    IEnumerator Upload(string username, float time, int numRooms) {
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("time", time.ToString());
+        form.AddField("rooms", numRooms.ToString());
+        byte[] rawData = form.data;
+        Debug.Log("Raw form data: " + System.Text.Encoding.UTF8.GetString(rawData));
+        UnityWebRequest www = UnityWebRequest.Post(url + "/add", form);
+        www.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success) {
+            errorText.SetActive(true);
+            Debug.Log(www.error);
+        } else {
+            SceneManager.LoadScene("Menu");
+        }
     }
 
     public void BackToMenu()
--- Assets/Scripts/TimerInfo.cs
+++ Assets/Scripts/TimerInfo.cs
@@ -4,8 +4,8 @@ public class TimerInfo : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public static float time;
-    public static string timeText;
+    public static float time = 0;
+    public static string timeText = "00:00:00.00";
     void Start()
     {
         
