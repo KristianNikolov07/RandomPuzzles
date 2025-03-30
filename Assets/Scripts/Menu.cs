dea52cc90cb025b9538417b1634e9d0918468fc2
KNProNooob <138398165+KNProNooob@users.noreply.github.com>
2025-03-30 15:47:46 +0300

    Added an option to select how much rooms the generate

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.Rendering;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject mainMenuPanel;
+    public GameObject gameSettingsPanel;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,8 +21,28 @@ public class Menu : MonoBehaviour
     }
 
     public void Play(){
-        Debug.Log("aaa");
-        SceneManager.LoadScene(1);
+        mainMenuPanel.SetActive(false);
+        gameSettingsPanel.SetActive(true);
+    }
+
+    public void Gen10Rooms(){
+        GameSettings.numRooms = 10;
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Gen20Rooms(){
+        GameSettings.numRooms = 20;
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Gen50Rooms(){
+        GameSettings.numRooms = 50;
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Back(){
+        gameSettingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
     }
 
     public void Leaderboard(){
